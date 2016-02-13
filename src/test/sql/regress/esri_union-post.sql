efe492fe018515f445c3be565dd7292cf09bec2b
Lars Aksel Opsahl <lop@skogoglandskap.no>
2016-02-13 01:04:31 +0100

    Added basic test

--- src/test/sql/regress/esri_union-post.sql
+++ src/test/sql/regress/esri_union-post.sql
@@ -1,2 +1,35 @@
 
 drop schema sl_lop cascade;
+
+DROP FUNCTION esri_union_create_tmp_tables (scp esri_union_single_cell_pameter);
+
+DROP FUNCTION esri_union_intersection(g1 geometry,g2 geometry);
+
+DROP FUNCTION valid_multipolygon_difference(g1 geometry,g2 geometry);
+
+DROP FUNCTION esri_union_remove_grid (	result_table text,
+	pk_key_one text,
+	pk_key_two text);
+	
+DROP FUNCTION get_esri_union_cell (
+scp esri_union_single_cell_pameter, 
+create_tmp_table boolean);
+
+DROP FUNCTION get_esri_union (
+input_table_one text, 
+input_table_two text,
+result_table_name text,
+max_rows_pr_cell integer );
+
+DROP FUNCTION cbg_get_table_extent (schema_table_name_column_name_array VARCHAR[]);
+
+DROP FUNCTION cbg_content_based_balanced_grid (	
+													table_name_column_name_array VARCHAR[], 
+													grid_geom_in geometry,
+													min_distance integer,
+													max_rows integer);	
+DROP FUNCTION cbg_content_based_balanced_grid(
+													table_name_column_name_array VARCHAR[],
+													max_rows integer); 
+													
+DROP TYPE IF EXISTS esri_union_single_cell_pameter cascade;
--- src/test/sql/regress/esri_union.sql
+++ src/test/sql/regress/esri_union.sql
@@ -1,2 +1,5 @@
 -- Repeat all tests with the new function names.
-SELECT '1', '1'
+SELECT '1', count(*) from sl_lop.ar250_flate_rute_3_flate;
+SELECT '2', count(*) from sl_lop.beitebruk;
+SELECT '3', count(*) from sl_lop.ar250_flate_rute_3_flate a, sl_lop.beitebruk b WHERE ST_Intersects(a.geo,b.geom);
+SELECT '4', get_esri_union('sl_lop.ar250_flate_rute_3_flate sl_sdeid geo','sl_lop.beitebruk gid geom','sl_lop.res_beite_bruk',500);
