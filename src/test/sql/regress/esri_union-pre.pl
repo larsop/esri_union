b8f58c0131f9c611f3a9b43f3bbe3463241f588c
Lars Aksel Opsahl <lop@skogoglandskap.no>
2016-02-13 09:26:26 +0100

    Cleaned up test script

--- src/test/sql/regress/esri_union-pre.pl
+++ src/test/sql/regress/esri_union-pre.pl
@@ -2,59 +2,32 @@
 use File::Copy;
 use File::Spec::Functions;
 
-my ($pre_filename) = @ARGV;
-
-
-print "\n Output file is $pre_filename \n";
-
 $ESRI_UNION='esri_union-pre.sql';
+print "\n Output file is $ESRI_UNION \n";
 
-if ( -e '/Users/lop/dev/github/esri_union/src/main/sql/function_01_esri_unio_create_tmp_tables.sql' ) 
+open($fh_out, ">", $ESRI_UNION);
+
+# get funtion defs for esri union based grid
+for my $file (glob '../../../main/sql/func*') {
+	copy_file_into($file,$fh_out);
+}
+
+# get def for conetnet based grid
+if ( -e '/Users/lop/dev/github/content_balanced_grid/func_grid' ) 
 {
-
-
-	print "Source file exist so we can create a new $ESRI_UNION \n";
-	# We need the spatial ref. for this tests 
-	open($fh_out, ">", $ESRI_UNION);
-
-	# TODO find another way to to get data from github
-	for my $file (glob '/Users/lop/dev/github/esri_union/src/main/sql/func*.sql') {
-		copy_file_into($file,$fh_out);
-	}
-
 	# TODO find another way to to get data from github
 	for my $file (glob '/Users/lop/dev/github/content_balanced_grid/func_grid/func*.sql') {
 		copy_file_into($file,$fh_out);
 	}
-
-	copy_file_into('/Users/lop/dev/github/esri_union/src/test/sql/regress/esri_union-pre-data.sql',$fh_out);
-	
-	close($fh_out);	 
-	
-	
 } 
 else
 {
-	print "Source file use the old one $ESRI_UNION \n";
+	copy_file_into('esri_union-pre-cbg-def.sql',$fh_out);
+	print "use the esri_union-pre-cbg-def.sql \n";
 }
 
-# build up topo_update.sql file used for test
-open($fh_out_final, ">", $pre_filename);
-
-copy_file_into($ESRI_UNION,$fh_out_final);
-
-copy_file_into('../../../main/sql/topo_update/schema_topo_update.sql',$fh_out_final);
-
-for my $file (glob '../../../main/sql/topo_update/schema_userdef*') {
-	copy_file_into($file,$fh_out_final);
-}
-
-for my $file (glob '../../../main/sql/topo_update/function*') {
-	copy_file_into($file,$fh_out_final);
-}
-
-close($fh_out_final);	 
-
+copy_file_into('esri_union-pre-data.sql',$fh_out);
+close($fh_out);	 
 
 sub copy_file_into() { 
 	my ($v1, $v2) = @_;
