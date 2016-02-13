857bbd84d00d0b6e53310dce9c54ba92d4e13ece
Lars Aksel Opsahl <lop@skogoglandskap.no>
2016-02-13 17:37:22 +0100

    Added more testing

--- src/test/sql/regress/esri_union.sql
+++ src/test/sql/regress/esri_union.sql
@@ -5,4 +5,9 @@ SELECT '3', count(*) from sl_lop.ar250_flate_rute_3_flate a, sl_lop.sk_grl_flate
 SELECT '4', get_esri_union('sl_lop.ar250_flate_rute_3_flate sl_sdeid geo','sl_lop.sk_grl_flate komid geo','sl_lop.res_ar250_sk_grl',500,'sl_lop.grid_ar250_sk_grl');
 SELECT '5', count(*) from sl_lop.res_ar250_sk_grl;
 SELECT '6', Sum(ST_area(geom)) from sl_lop.res_ar250_sk_grl;
+SELECT '7', Sum(ST_area(a.geom)) AS areal_new from sl_lop.res_ar250_sk_grl a WHERE a.t1_sl_sdeid  is not null;
+SELECT '8', Sum(ST_area(b.geo)) from sl_lop.ar250_flate_rute_3_flate b;
+SELECT '9', Sum(ST_area(a.geom)) from sl_lop.res_ar250_sk_grl a WHERE a.t2_komid  is not null;
+SELECT '10', Sum(ST_area(b.geo)) from sl_lop.sk_grl_flate b;
+SELECT '11', Sum(ST_area(a.geom)) AS areal_new from sl_lop.res_ar250_sk_grl a WHERE a.t1_sl_sdeid  is not null AND a.t2_komid  is not null;
 
